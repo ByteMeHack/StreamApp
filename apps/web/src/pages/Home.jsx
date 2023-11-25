b0ebc9f613a8ea9be35e1944b0e74414e5eb450e
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-25 21:20:21 +0300

    fix: ui

--- apps/web/src/pages/Home.jsx
+++ apps/web/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { roomsSelector, userSelector } from "../store/selectors";
 import { setRooms } from "../store/roomSlice";
 import { Stack } from "@chakra-ui/react";
+import ModalCreateRoom from "../components/ModalCreateRoom";
 
 export default function Home() {
   const dispatch = useDispatch();
