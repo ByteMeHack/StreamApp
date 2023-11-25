92dba2fdbdedb56045175158ad7a1f122f8bc418
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-25 23:45:14 +0300

    fix: enter pass

--- apps/web/src/components/ModalEnterRoom.jsx
+++ apps/web/src/components/ModalEnterRoom.jsx
@@ -6,23 +6,25 @@ import { Button, Input, Stack } from "@chakra-ui/react";
 export default function ModalEnterRoom({ id }) {
   const [pass, setPass] = useState("");
   const navigate = useNavigate();
-  <Stack direction="row" spacing={10} width={300} placeContent="center">
-    <Input
-      autocomplete="off"
-      placeholder="Name of room"
-      type="text"
-      isRequired
-      onChange={(e) => setPass(e.target.value)}
-      borderColor="#e02525"
-    />
-    <Button
-      onClick={async () => {
-        await registerToRoom(id, pass).then((res) => {
-          navigate(`/rooms/${res.id}`);
-        });
-      }}
-    >
-      Enter
-    </Button>
-  </Stack>;
+  return (
+    <Stack direction="row" spacing={10} width={300} placeContent="center">
+      <Input
+        autocomplete="off"
+        placeholder="Name of room"
+        type="text"
+        isRequired
+        onChange={(e) => setPass(e.target.value)}
+        borderColor="#e02525"
+      />
+      <Button
+        onClick={async () => {
+          await registerToRoom(id, pass).then((res) => {
+            navigate(`/rooms/${res.id}`);
+          });
+        }}
+      >
+        Enter
+      </Button>
+    </Stack>
+  );
 }
