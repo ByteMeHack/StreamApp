3c21471c153e6a0f77c047ecea75b6475818cbef
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 01:09:59 +0300

    feat: chat

--- apps/web/src/components/Chat.jsx
+++ apps/web/src/components/Chat.jsx
@@ -1,11 +1,41 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Button, CardBody, Heading, Input, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 
-export default function Chat() {
+export default function Chat({ room_id }) {
+  const [message, setMessage] = useState("");
+  const socket = new WebSocket(`ws://bytemehack.ru/api/room/${room_id}`);
+  socket.onmessage = (event) => {
+    console.log(event.data);
+  };
   return (
     <Box className="blackBlock" p={3}>
-      <Heading size="sm" color="white" className="grayblock">
-        Chat
-      </Heading>
+      <Card>
+        <Heading size="sm" color="white" className="grayblock">
+          Chat
+        </Heading>
+        <CardBody bgColor="gray">
+          <Stack>Elements</Stack>
+          <Stack direction="row" spacing={3}>
+            <Input
+              placeholder="Type message here..."
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <Button
+              onClick={() => {
+                socket.send(
+                  JSON.stringify({
+                    user_id: 1,
+                    message_type: 1,
+                    contents: message,
+                  })
+                );
+              }}
+            >
+              Send
+            </Button>
+          </Stack>
+        </CardBody>
+      </Card>
     </Box>
   );
 }
