deaf436ecdac487fd688da67e430197bb9788f2a
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 10:27:54 +0300

    fix: message

--- apps/web/src/components/Chat.jsx
+++ apps/web/src/components/Chat.jsx
@@ -38,6 +38,7 @@ export default function Chat({ room_id }) {
           <Input
             placeholder="Type message here..."
             onChange={(e) => setMessage(e.target.value)}
+            value={message}
           />
           <Button isDisabled={socketRef.current === null} onClick={sendMessage}>
             Send
