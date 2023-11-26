c36ac5269585345c91fe500fb994573a16900b71
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 03:34:14 +0300

    fix: ui

--- apps/web/src/components/Message.jsx
+++ apps/web/src/components/Message.jsx
@@ -3,15 +3,16 @@ import { Stack, Text } from "@chakra-ui/react";
 export default function Message({ message }) {
   return (
     <Stack
-      gap={5}
+      gap={2}
       display="flex"
       flexDirection="column"
       width={300}
       height={100}
+      bgColor="gray"
     >
-      <Text>{message.user_id}</Text>
-      <Text>{message.contents}</Text>
-      <Text>{message.timestamp}</Text>
+      <Text>User: {message.user_id}</Text>
+      <Text>Message: {message.contents}</Text>
+      <Text>Date:{message.timestamp}</Text>
     </Stack>
   );
 }
