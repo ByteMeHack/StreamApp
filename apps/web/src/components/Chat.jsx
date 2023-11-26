9b94ca434fd2578042a200b07da9b50006c7234f
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 11:19:40 +0300

    fix: ui chat

--- apps/web/src/components/Chat.jsx
+++ apps/web/src/components/Chat.jsx
@@ -29,7 +29,7 @@ export default function Chat({ room_id }) {
         <Heading size="sm" color="white" className="grayblock">
           Chat
         </Heading>
-        <Stack overflow="scroll" height="50%">
+        <Stack overflow="scroll" height="50rem">
           {messages.map((message) => {
             return <Message key={message.contents} message={message} />;
           })}
