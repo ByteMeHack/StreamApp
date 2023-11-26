9abe4f0696b8e36f7935109fa49abf00c8759ca0
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 03:46:59 +0300

    fix: ui

--- apps/web/src/components/Message.jsx
+++ apps/web/src/components/Message.jsx
@@ -10,6 +10,7 @@ export default function Message({ message }) {
       height={100}
       bgColor="gray"
       borderRadius={10}
+      p={3}
     >
       <Text color="black">User: {message.user_id}</Text>
       <Text color="black">Message: {message.contents}</Text>
