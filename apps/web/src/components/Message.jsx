8e4dbeb940a41a234de3db65ed3989e949fc2acb
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 03:13:10 +0300

    debug

--- apps/web/src/components/Message.jsx
+++ apps/web/src/components/Message.jsx
@@ -1,11 +1,18 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 
 export default function Message({ message }) {
+  console.log(message);
   return (
-    <Box gap={5} display="flex" flexDirection="column" width={300} height={100}>
+    <Stack
+      gap={5}
+      display="flex"
+      flexDirection="column"
+      width={300}
+      height={100}
+    >
       <Text>{message.user_id}</Text>
       <Text>{message.contents}</Text>
       <Text>{message.timestamp}</Text>
-    </Box>
+    </Stack>
   );
 }
