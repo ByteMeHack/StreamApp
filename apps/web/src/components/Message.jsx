3a592d2bb52b76c02a6f8c16ed0eb49e7fa84eb0
Alyona Trofimova <atrofimova516@gmail.com>
2023-11-26 03:09:19 +0300

    fix: ui

--- apps/web/src/components/Message.jsx
+++ apps/web/src/components/Message.jsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function Message({ message }) {
   return (
-    <Box gap={5} display="flex" flexDirection="column">
+    <Box gap={5} display="flex" flexDirection="column" width={300} height={100}>
       <Text>{message.user_id}</Text>
       <Text>{message.contents}</Text>
       <Text>{message.timestamp}</Text>
